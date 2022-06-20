3864b0411ff9cd484e6296076ba17b2cbb4aece9
GovindSingh9447 <govindkrsingh0605@gmail.com>
2022-06-20 23:09:24 +0530

    Spinner

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
 <h2>App Components</h2>
+<NavBar/>
+<Spinner/>
 <router-view/>
 </template>
 
 <script>
+import NavBar from './components/NavBar.vue';
+import Spinner from './components/Spinner.vue';
  
     export default{
-      name: 'App'
-
- }
+    name: "App",
+    components: { NavBar, Spinner }
+}
 </script>
 
 bootstrap font-awesome axios
