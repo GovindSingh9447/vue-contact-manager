bb294496bc610073eb43753a51e762a375cd6ab9
GovindSingh9447 <govindkrsingh0605@gmail.com>
2022-06-20 23:27:33 +0530

    Add Contact

--- src/views/ContantManager.vue
+++ src/views/ContantManager.vue
@@ -1,5 +1,13 @@
 <template>
-  Contant Manager
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <p class="h3 text-success fw-bold">Contact Manager
+          <router-link to="/contacts/add" class="btn btn-success btn-sm"><i class="fa fa-plus-circle"></i> New</router-link>
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
