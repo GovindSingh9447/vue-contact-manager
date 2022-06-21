faad611bf7b46c6f6417c0773859123db389619b
GovindSingh9447 <govindkrsingh0605@gmail.com>
2022-06-21 18:20:59 +0530

    img

--- src/views/ContantManager.vue
+++ src/views/ContantManager.vue
@@ -30,13 +30,13 @@
           <div class="card-body">
             <div class="row">
               <div class="col-sm-4">
-
+                 <img src="https://imgs.search.brave.com/eg9ZWFBlvWt511V1bbSKOz-64P-j6X8f498x1uYpdxI/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5L/Ny0zQUFqRlZxUnR2/RGw3SUJYWWhBSGFI/YSZwaWQ9QXBp" alt="" class="contact-img">
               </div>
               <div class="col-sm-7">
 
               </div>
               <div class="col-sm-1">
-                
+
               </div>
             </div>
           </div>
