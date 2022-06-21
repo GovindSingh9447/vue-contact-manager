dfbeefff517a980dfaad8b52c18d63bf407b8a1f
GovindSingh9447 <govindkrsingh0605@gmail.com>
2022-06-21 18:48:24 +0530

    cards

--- src/views/ContantManager.vue
+++ src/views/ContantManager.vue
@@ -26,17 +26,29 @@
   <div class="container">
     <div class="row">
       <div class="col-md-6">
-        <div class="card my-2 list-group-item-success">
+        <div class="card my-2 list-group-item-success shadow-lg">
           <div class="card-body">
-            <div class="row">
+            <div class="row align-items-center">
               <div class="col-sm-4">
                  <img src="https://imgs.search.brave.com/eg9ZWFBlvWt511V1bbSKOz-64P-j6X8f498x1uYpdxI/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5L/Ny0zQUFqRlZxUnR2/RGw3SUJYWWhBSGFI/YSZwaWQ9QXBp" alt="" class="contact-img">
               </div>
               <div class="col-sm-7">
-
+                <ul class="list-group">
+                  <li class="list-group-item">Name: <span class="fw-bold">Name</span></li>
+                  <li class="list-group-item">Email: <span class="fw-bold">Email@name.com</span></li>
+                  <li class="list-group-item">Mobile: <span class="fw-bold">583639364</span></li>
+                </ul>
               </div>
-              <div class="col-sm-1">
-
+              <div class="col-sm-1 d-flex flex-column justify-content-center align-items-center">
+                <router-link to="/contacts/view/:contactId" class="btn btn-warning my-1">
+                  <i class="fa fa-eye"></i>
+                </router-link>
+                <router-link to="/contacts/view/:contactId" class="btn btn-primary my-1">
+                  <i class="fa fa-pen"></i>
+                </router-link>
+                <button class="btn btn-danger my-1">
+                  <i class="fa fa-trash"></i>
+                </button>
               </div>
             </div>
           </div>
